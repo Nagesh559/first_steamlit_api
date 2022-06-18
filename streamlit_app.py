3033d75a76b813d23250b5b8c0434014f91f9adb
Nagesh559 <72309506+Nagesh559@users.noreply.github.com>
2022-06-18 14:38:21 +0530

    Update streamlit_app.py
    added module for pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 
 import streamlit
+import pandas
  
 
 streamlit.header('Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_fruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruitlist)
