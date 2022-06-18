60f50d46671566c9815792d21c57ce40cf03560d
Nagesh559 <72309506+Nagesh559@users.noreply.github.com>
2022-06-18 14:08:21 +0530

    Update streamlit_app.py
    updated content for header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 
 import streamlit
+ 
 
-streamlit.title("My parents are healthy")
-
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Favorites')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
