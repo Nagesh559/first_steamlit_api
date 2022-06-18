71a8a2f7889807e9f006fb7a446080dc53e9943e
Nagesh559 <72309506+Nagesh559@users.noreply.github.com>
2022-06-18 14:45:51 +0530

    Update streamlit_app.py
    corrected spellings

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('BUILD YOUR OWN SOOMTHIE')
 my_fruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include
-steamlit.multiselect("Pick some fruits:", list(my_fruitlist.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruitlist.index))
 
 #display the table on the page 
 streamlit.dataframe(my_fruitlist)
