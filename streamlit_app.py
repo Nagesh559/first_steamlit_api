3c7bfad21951c6e36be20698d333f0f28c16dc7c
Nagesh559 <72309506+Nagesh559@users.noreply.github.com>
2022-06-18 14:44:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('BUILD YOUR OWN SOOMTHIE')
-my_fruitlist)) = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include
 steamlit.multiselect("Picl some fruits:", list(my_fruitlist))
 
