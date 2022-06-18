68d2c7592f9ce0cc73775ff01ae39400aeed38ff
Nagesh559 <72309506+Nagesh559@users.noreply.github.com>
2022-06-18 14:39:29 +0530

    Update streamlit_app.py
    ADDED HEADER

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.header('BUILD YOUR OWN SOOMTHIE')
 my_fruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruitlist)
